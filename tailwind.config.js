578d5f83d15fd5828cb7eecd8e046339f5059bfb
Mohabexe <s22512065eg@std.aou.edu.eg>
2024-11-07 16:57:35 +0200

    Add animations for elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { transform } = require("sucrase")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./build/*.{html,js}"],
@@ -15,9 +17,19 @@ module.exports = {
           "75%": { top: "-3.6em" },
           "100%": { top: "0" },
         },
+        left: {
+          "0%": { transform: "translateX(-300%)", opacity: 0 },
+          "100%": { transform: "translateX(0)", opacity: 1 },
+        },
+        right: {
+          "0%": { transform: "translateX(30%)", opacity: 0 },
+          "100%": { transform: "translateX(0)", opacity: 1 },
+        },
       },
       animation: {
         scroll: "scroll 7s infinite",
+        left: "left 0.5s ease-in-out",
+        right: "right 0.5s ease-in-out",
       },
     },
   },
